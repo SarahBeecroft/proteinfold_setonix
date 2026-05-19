7fafe7525508d870e61f2a25e91774e54d163fc2
SarahBeecroft <Sarah.beecroft@pawsey.org.au>
2026-05-19 12:50:58 +0800

    Add SLURM job script for ProteinFold execution
    This script sets up a SLURM job for running ProteinFold using Nextflow with specified configurations and resources.

---
+++ example_run_proteinfold.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l 
+#SBATCH --job-name=proteinfold
+#SBATCH --time=5:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=8GB
+
+module load singularity/3.11.4-nompi 
+source  /software/projects/pawsey0001/sbeecroft/miniforge3/bin/activate base
+
+nextflow run main.nf \
+    --input samplesheet.csv \
+    --outdir outdir \
+    -config conf/pawsey_setonix.conf \
+    -config conf/amd_containers.conf \
+    --mode alphafold2 \
+    -resume
