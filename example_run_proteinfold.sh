9150409b17312a6ed9059126e25eaf73a30a71ab
SarahBeecroft <Sarah.beecroft@pawsey.org.au>
2026-06-17 15:15:55 +0800

    Update output directory for Nextflow workflow

--- example_run_proteinfold.sh
+++ example_run_proteinfold.sh
@@ -7,15 +7,11 @@
 #SBATCH --mem=8GB
 #SBATCH --account=pawsey0012
 
-module load singularity/3.11.4-nompi 
 
-# This workflow seems to only work with Nextflow version 25.10.4, which I installed in my base conda env. 
-# If you choose to also install with conda, you'll need to update the example path below with your conda info
-source  /software/projects/pawsey0001/sbeecroft/miniforge3/bin/activate base
-
-export NXF_SINGULARITY_CACHEDIR=$MYSCRATCH/nf_singularity_cache
-
-#Check if FCP is installed. If not, install it. This only needs to be done once. 
+############################################################################################################################################################
+# Check if FCP is installed. If not, install it. This only needs to be done once.
+# Once FCP is installed for you, you can delete this block.
+# FCP enables super-fast copy of key reference files to /tmp for colabfold_search. Reading those files from /tmp instead of /scratch provides a 10x speedup. 
 if ! command -v fcp >/dev/null 2>&1; then
     echo "fcp not found on PATH" >&2
     module load rust/1.85.0
@@ -23,10 +19,21 @@ if ! command -v fcp >/dev/null 2>&1; then
     echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> ~/.bashrc
     source ~/.bashrc
 fi 
+############################################################################################################################################################
 
+
+# Load singularity module.
+module load singularity/3.11.4-nompi 
+
+# This workflow seems to only work with Nextflow version 25.10.4, which I installed in my conda base environment. 
+# If you choose to install into a different conda environment, you'll need to replace `base` with your environment name
+conda_activate="${CONDA_EXE%conda}activate"
+source $conda_activate base
+
+# Run the workflow
 nextflow run main.nf \
     --input samplesheet.csv \
-    --outdir outdir \
+    --outdir $MYSCRATCH/proteinfold/outdir \
     -config conf/pawsey_setonix.conf \
     -config conf/amd_containers.conf \
     --mode boltz,colabfold,esmfold,alphafold2 \
